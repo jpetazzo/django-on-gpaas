658fa8c6f1b504bafad57b6497a8a9661a4e69a0
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2015-04-12 16:12:17 -0700

    3rd time is a champ

---
+++ paas.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+if ! [ -f credentials.gandi ]; then
+    echo "Create the 'credentials.gandi' file to define UID, PASSWORD, and GITHOST"
+    echo "Example:"
+    echo "cat >credentials.gandi <<EOF"
+    echo "LOGIN=123456"
+    echo "PASSWORD=ABCD1234"
+    echo "DC=dc1"
+    echo "EOF"
+    exit 1
+fi
+. ./credentials.gandi
+
+case $1 in
+console)
+    ssh $LOGIN@console.$DC.gpaas.net
+    ;;
+deploy)
+    git remote | grep -q ^gandi$ || git remote add gandi ssh+git://$LOGIN@git.$DC.gpaas.net/default.git
+    git push -f gandi master
+    ssh $LOGIN@git.$DC.gpaas.net "deploy default"
+    ;;
+logs)
+    echo "cd /lamp0/var/log/www/" > get-logs.sftp
+    echo "get uwsgi.log" >> get-logs.sftp
+    sftp -b get-logs.sftp $LOGIN@git.$DC.gpaas.net
+    tail -n 50 uwsgi.log
+    rm get-logs.sftp
+    ;;
+*)
+    echo "Specify one command: console, deploy, logs."
+    ;;
+esac
+
+
--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import os
 import os.path
  
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                'cms')))
+                                                'project')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
