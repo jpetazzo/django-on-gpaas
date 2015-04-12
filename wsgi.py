4864754ea8aa21d77ebe8bab70bf618225e64d4f
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2015-04-12 15:22:51 -0700

    fix wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,7 @@ import sys
 import os
 import os.path
  
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
-#                                                'cms')))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
