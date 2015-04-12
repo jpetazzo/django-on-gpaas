b5da591e516043d8764c60b8aa33053bbdb01f6e
Jerome Petazzoni <jerome.petazzoni@gmail.com>
2015-04-12 15:27:17 -0700

    woot2

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ import sys
 import os
 import os.path
  
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                'cms')))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
